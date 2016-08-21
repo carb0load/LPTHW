ce66831756b41d04b941e9051bb17b269987ab34
Daniel <dsislo@gmail.com>
2016-08-21 16:33:59 -0400

    Exercises 3 and 4
    Adding exercises 3 and 4 to the repository.

---
+++ Exercises/Exercise 3.py
@@ -0,0 +1,23 @@
+print "I will now count my chickens:" # General idea of the program
+
+print "Hens", 25 +30 / 6 # This calculates the number of hens
+print "Roosters", 100- 25 * 3 % 4 # This calculates the number of roosters
+
+print "Now I will count the eggs:" # Explaining what is coming next
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 # The $ is called modulus and is basically: X / Y with J remaining
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3+ 2 < 5 - 7 # This returns False since it is not correct
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
